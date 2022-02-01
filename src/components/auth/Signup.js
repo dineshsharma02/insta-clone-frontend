8cfd8ea59c088bf2f5a3da88b53ba386fd59505c
dineshsharma02 <62809636+dineshsharma02@users.noreply.github.com>
2022-02-01 15:56:07 +0530

    Signup frontend connected and tested

--- src/components/auth/Signup.js
+++ src/components/auth/Signup.js
@@ -6,7 +6,8 @@ import { useState } from 'react';
 const Signup = () => {
     const [credential, setCredential] = useState({
         username:"",
-        name:"",
+        first_name:"",
+        last_name:"",
         email:"",
         password:"",
         cpassword:"",
@@ -14,26 +15,37 @@ const Signup = () => {
 
     const handleRegister=  async(e)=>{
         e.preventDefault();
-        const response = await fetch("http://127.0.0.1:8000/accounts/",{
+        const response = await fetch("http://127.0.0.1:8000/accounts/register/",{
             method:"POST",
             headers:{
                 "Content-type":"application/json",
             },
             body:JSON.stringify({
-                first_name:credential.fullname,
+                first_name:credential.first_name,
+                last_name:credential.last_name,
                 username:credential.username,
                 email:credential.email,
                 password:credential.password,
+                password2:credential.cpassword
             })
         });
         const json = await response.json()
+        console.log(response.status)
         console.log(json);
+        if (response.status===201){
+            console.log("User created");
+        }
+        else{
+            console.log(response);
+        }
+
 
 
     }
 
     const onChange=(e)=>{
         setCredential({ ...credential,[e.target.name]:e.target.value});
+        
     }
 
 
@@ -45,8 +57,12 @@ const Signup = () => {
       <input type="email" name="email" id="email" onChange={onChange} />
       <br />
       <br />
-      <label htmlFor="fullname">Full Name</label>
-      <input type="text" name="username" id="username" onChange={onChange} />
+      <label htmlFor="fullname">First Name</label>
+      <input type="text" name="first_name" id="first_name" onChange={onChange} />
+      <br />
+      <br />
+      <label htmlFor="fullname">Last Name</label>
+      <input type="text" name="last_name" id="last_name" onChange={onChange} />
       <br />
       <br />
       <label htmlFor="Name">Username</label>
@@ -55,6 +71,9 @@ const Signup = () => {
       <br />
       <label htmlFor="Name">Password</label>
       <input type="text" name="password" id="password" onChange={onChange} />
+      <br /><br />
+      <label htmlFor="Name">Confirm Password</label>
+      <input type="text" name="cpassword" id="cpassword" onChange={onChange} />
       <br /><br />
       <input type="submit" value="Register" />
       </form>
