5b8f0fd7f28e8c6e0c9409a60b8f6d5914498240
dineshsharma02 <62809636+dineshsharma02@users.noreply.github.com>
2022-03-03 23:58:49 +0530

    connected userinfo api with page

--- src/components/user/UserInfo.js
+++ src/components/user/UserInfo.js
@@ -1,7 +1,76 @@
-import React from "react";
+import axios from "axios";
+import React,{ useEffect,useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Feeditem from "../feed/Feeditem";
 import Me from "../Utils/Me";
 
 const UserInfo = () => {
+  let history = useNavigate()
+  const [userData, setUserData] = useState([])
+  // let [postdata, setPostdata] = useState([])
+  // useEffect(()=> {
+  //   async function fetchUserInfo(){
+  //     // await axios.get('http://127.0.0.1:8000/userposts/1/')
+  //     const response = await fetch('http://127.0.0.1:8000/userposts/1/')
+  //     .then(res=>{
+
+        
+  //       console.log(res.data);
+  //       console.log(res.json());
+  //       // setUserData(res.data)
+  //       // setPostdata((res.data))
+  //       // console.log(userData.length);
+  //     })
+  //     .catch(error=>{
+  //       console.log(error);
+  //     })
+  //   }
+  //   fetchUserInfo()
+  //   console.log("this is postdata::",userData);
+  //       console.log(userData.length);
+
+  
+  //   // return () => {
+  //   //   second
+  //   // }
+  // }, [])
+
+
+
+  useEffect(async() => {
+    let token = localStorage['authtoken']
+    console.log(token);
+    if (!token){
+      history("/login",{replace:true})
+    }
+    const response = await fetch("http://127.0.0.1:8000/userposts/1/",{
+            method:"GET",
+            headers:{
+                "Content-type":"application/json",
+                'Authorization': `Bearer ${token}`
+            },
+            
+        });
+
+        const json = await response.json()
+        console.log(response.status)
+        // console.log(response);
+        console.log(json);
+        if (response.status===200){
+            console.log("Posts fetched");
+            setUserData(json)
+            // setPosts(json)
+            
+        }
+        else{
+            console.log(response);
+        }
+        
+  }, [])
+  
+  
+
+
   return (
     <div className="main-container">
       <div className="info-section">
@@ -9,15 +78,15 @@ const UserInfo = () => {
           <Me></Me>
         </div>
         <div className="user-details">
-          <h2>dinesh.sharma02</h2>
+          <h2>{userData[0].username}</h2>
           <div className="user-follow">
-            <li>3 Posts</li>
+            <li>{userData.length} Posts</li>
             <li>55 followers</li>
             <li>83 following</li>
           </div>
 
           <div className="username-caption-container">
-          <h4>Dinesh Sharma</h4>
+          <h4>{userData[0].username}</h4>
           <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae illo nobis, hic aperiam pariatur omnis perspiciatis natus excepturi aspernatur explicabo unde, saepe quo!</p>
           </div>
           
