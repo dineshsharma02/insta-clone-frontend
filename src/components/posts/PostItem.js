b9de63f6711486e559c70dd7ff55d4499e3458f7
dineshsharma02 <62809636+dineshsharma02@users.noreply.github.com>
2022-03-06 17:17:43 +0530

    Styling post item to show likes and comment count

--- src/components/posts/PostItem.js
+++ src/components/posts/PostItem.js
@@ -1,13 +1,26 @@
 import React from 'react'
 import profpic from "../Utils/prof-image.jpg"
 const PostItem = (props) => {
-    let {id , image,likes,comment,caption} = props
+    let {id , image,likes,comments,caption} = props
     console.log(id,likes,caption);
   return (
       
-    <div className="post-collage-item">
-        <img src={image} alt="not found" width={'250px'} height={'300px'}/>
+    <>
+    <div className="image-card">
+    <img className='post-image' src={image} alt="not found" width={'250px'} height={'300px'}/>
+    <div className="likes-card">
+      <p>
+      <i class="fas fa-heart icons"></i> {likes}
+      
+      <i class="fas fa-comment icons"></i> {comments}</p>
     </div>
+    </div>
+    
+
+    {/* <div className="post-collage-item">
+        <img src={image} alt="not found" width={'250px'} height={'300px'}/>
+    </div> */}
+    </>
   )
 }
 
