e847af9e55f516cdaacfd37c5855826dad7bdbf8
dineshsharma02 <62809636+dineshsharma02@users.noreply.github.com>
2022-03-20 23:17:46 +0530

    Chat UI design added

---
+++ src/components/chat/AllMessages.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const AllMessages = () => {
+  return (
+    <div className="chat-container">
+      <div className="left-container">
+        <div className="username-area">
+          {username}
+        </div>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod alias blanditiis temporibus vero! Animi iste, placeat nihil asperiores molestias accusamus sit eius provident sequi voluptatum ipsam! Veniam error esse laboriosam. Et hic adipisci laboriosam fugiat maxime autem magni quod asperiores fuga ullam? Dolorum aperiam provident nobis cupiditate sapiente doloremque nam, corporis mollitia tempore doloribus maxime ad aliquid rem laudantium exercitationem veritatis, similique consequuntur dolor optio quaerat libero aliquam placeat error tempora. Eum et tempora aut sequi nesciunt sint ab doloribus cupiditate optio illo eaque tenetur non nostrum quae, vel ea! Aspernatur, unde totam blanditiis pariatur quis repellendus labore qui magni.
+      </div>
+      <div className="right-container">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, eum facilis repellat temporibus quos laudantium numquam tempora! Impedit vitae earum quam expedita nulla quas porro officia in id enim, veniam non eligendi nisi laborum! Sint iusto ducimus vero, nobis excepturi quae beatae sapiente consequatur animi consectetur facere odio! Inventore voluptatem maxime eum quas vero accusamus, repellat eaque reiciendis quos odit. Saepe nemo in dolores reiciendis architecto repellendus, modi veniam? Modi culpa inventore asperiores, quis qui ipsam iusto officia amet similique magnam nihil quo, explicabo velit, sint voluptatibus minima ab illo! Corrupti doloribus quia aspernatur reiciendis nesciunt numquam nihil quam ducimus voluptas consequuntur expedita ut deserunt consequatur laborum, quas deleniti doloremque excepturi fugit repudiandae veniam impedit itaque quos. Eaque atque numquam suscipit asperiores doloribus. Eveniet nemo autem natus sint quasi tempora quod deserunt? Nisi, officia nulla. Eligendi, blanditiis. Reiciendis nam tempora eum quas repellendus quae dicta expedita, a ex nemo incidunt? Ipsa dolor architecto voluptate, adipisci neque vitae quidem! Dolorem et recusandae ut dolorum ipsa dolore quos debitis aperiam ratione quasi! Voluptatem explicabo earum dolorum facilis! Reprehenderit perspiciatis error sit nam fugit cum. Quia odio libero placeat non, sed odit? Adipisci voluptates temporibus blanditiis quisquam aperiam minus dolores cupiditate excepturi ut sit quia magnam, rem ducimus neque vel. Et quidem ad ea velit possimus cum quo aperiam doloremque tempora magni nam dolorum voluptatem temporibus quis beatae inventore dicta quia, eligendi eaque ratione incidunt neque itaque reiciendis! Laudantium, facilis molestiae. Doloremque illo voluptatum inventore neque voluptatem odio provident hic quae minus rem eveniet exercitationem distinctio ratione aperiam nisi eligendi est beatae iure alias repellat, ducimus commodi optio impedit? In, expedita! Reprehenderit doloribus rem tenetur incidunt temporibus inventore corrupti cupiditate obcaecati sequi delectus vitae, rerum sapiente odio perspiciatis quibusdam porro at saepe aperiam. Illum excepturi officiis aliquam dolor! Dignissimos laudantium necessitatibus quam nulla?
+      </div>
+    </div>
+  )
+}
+
+export default AllMessages
--- src/components/chat/ChatHome.js
+++ src/components/chat/ChatHome.js
@@ -1,13 +1,21 @@
 import React from 'react'
 
 const ChatHome = () => {
+  const username = localStorage.getItem('username')
   return (
-    <div className="chat-container">
-      <div className="left-container">
-        hello
+    <div className="chat-grid-container">
+      <div className="username-area">
+        {username}
       </div>
-      <div className="right-container">
-        hii
+      <div className="response-username-area">
+        user
+      </div>
+      <div className="all-messages-area">
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero commodi aliquam, nesciunt beatae hic et eligendi, quidem animi itaque ut quis delectus? Ad impedit reiciendis voluptatem quidem laborum ipsam est.
+      </div>
+      <div className="message-summ-area">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci quaerat veniam dolor illo incidunt eum voluptas ullam ipsam perspiciatis quo atque nostrum ex illum eveniet, accusamus sit sequi id, minima aspernatur modi nobis, harum eligendi! Perspiciatis consectetur repellendus est. Consectetur atque inventore hic quidem vitae, odit veritatis aspernatur blanditiis eveniet, ab aliquid fugit cumque corporis, ratione in? Pariatur ex aliquid harum exercitationem nostrum odio itaque, ratione dicta nobis? Recusandae dicta pariatur id atque enim rerum exercitationem quo! Corporis facere quos delectus, veritatis explicabo pariatur labore molestiae impedit blanditiis accusamus? Odit eum sit optio autem nostrum possimus illo architecto quidem nihil nisi beatae, minus cumque dolores veniam eos praesentium nulla. Itaque consequuntur sint, sequi eligendi neque incidunt facere magni quis voluptates esse laboriosam praesentium blanditiis delectus, ipsam corrupti fugiat doloribus alias temporibus asperiores rem provident quas distinctio inventore? Nisi modi aliquid reprehenderit autem sunt fugit voluptatibus eveniet porro ducimus! Molestiae, vero accusamus magnam quaerat minima rerum sunt dolorem minus quisquam atque voluptas quasi cumque! Tempore maiores nulla aliquam. Dolorem dolor eaque natus nulla officiis exercitationem facere reiciendis hic. Velit odio totam dolorum harum, placeat quisquam quasi nisi neque pariatur repudiandae voluptate amet beatae maiores non? Incidunt odio consequatur animi numquam temporibus.
+
       </div>
     </div>
   )
